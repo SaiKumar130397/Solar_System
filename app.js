f3793fcb8dfe390da4068d96523c955e6296568d
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-02-06 17:20:31 +0530

    updated app.js file

--- app.js
+++ app.js
@@ -91,12 +91,7 @@ app.get('/ready',   function(req, res) {
     });
 })
 
-if (require.main === module) {
-    app.listen(3000, () => {
-        console.log("Server successfully running on port - 3000");
-    });
-}
-
+app.listen(3000, () => { console.log("Server successfully running on port - " +3000); })
 module.exports = app;
 
 //module.exports.handler = serverless(app)
