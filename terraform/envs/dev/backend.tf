1347716a7d56742362ed75a2cc128e47593e7ff1
Sai Kumar <kumarsaisurisetty333@gmail.com>
2026-03-20 12:04:01 +0530

    removed dynamodb

--- terraform/envs/dev/backend.tf
+++ terraform/envs/dev/backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket         = "solarsystem-tf-state"
     key            = "dev/terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "solar-system-tf-lock"
     encrypt        = true
   }
 }
